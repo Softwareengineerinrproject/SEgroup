c02b333abfe215b07a5a93d9318b9e35a41e4462
Sahitreddych <112913170+Sahitreddych@users.noreply.github.com>
2022-11-07 23:35:44 -0600

    phase2

---
+++ Source Code/Phase 2/PrecautionsActivity.java
@@ -0,0 +1,14 @@
+package com.example.covid19brainstromers;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PrecautionsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_precautions);
+    }
+}
